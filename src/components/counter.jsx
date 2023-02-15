2e2adb7c418b55bd68a2e084811f67e711d70769
fmfahath.testLab <fmfahath.dev@gmail.com>
2023-02-15 16:46:39 +0545

    Counter module's class & methods added / rendered

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,17 +1,55 @@
 import React, { Component } from 'react';
 
-class Counter extends Component {   
-    // state = {  } 
+class Counter extends Component { 
+     
+    //property of Component class - state is a Special property - [its an object]
+    state = {   
+        count: 0,    //propery
+        address:{   //propery
+            HomeNo: "",
+            Street: "",
+        }
+     }; 
+
+
+     //Methods of Component class
+     formatCount(){
+        const {count} = this.state; //object de-structure
+        // return this.state.count === 0 ? "Zero" : this.state.count;
+        return count === 0 ? "Zero" : count;
+
+        // const x = <h1>Zero</h1>;
+        // return count === 0 ? <h1>Zero</h1> : count;
+        // return count === 0 ? x : count;
+
+     }
+
+     //---------------------------------------------------------------------------------
+
     // render() { 
     //     return <div><h1>Hello React.js</h1><button>Increment</button></div>;
     // }
 
-    render(){
+    //------------------------------------------------------------------------------------
+
+    // render(){
+    //     return (
+    //         <React.Fragment>
+    //             <h1>Hello React.js</h1>
+    //             <button>Increment</button>
+    //         </React.Fragment>
+    //     );
+    // }
+
+     //------------------------------------------------------------------------------------
+
+     render(){
         return (
-            <React.Fragment>
-                <h1>Hello React.js</h1>
+            <div>
+                {/* <span>{this.state.count}</span> */} 
+                <span>{this.formatCount()}</span>
                 <button>Increment</button>
-            </React.Fragment>
+            </div>
         );
     }
 }
