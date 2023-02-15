d93fb94bb7143ebdb85a13350e3900c54961bbbc
fmfahath.testLab <fmfahath.dev@gmail.com>
2023-02-15 16:16:51 +0545

    render's return code intention format

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 
 class Counter extends Component {   
     // state = {  } 
-    render() { 
-        return <h1>Hello React.js</h1>;
+    // render() { 
+    //     return <div><h1>Hello React.js</h1><button>Increment</button></div>;
+    // }
+
+    render(){
+        return (
+            <div>
+                <h1>Hello React.js</h1>
+                <button>Increment</button>
+            </div>
+        );
     }
 }
  
